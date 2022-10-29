42dc43cccca011ee4fb36119ba02d95bae2e5620
Duncan Tang <pofungt@hotmail.com>
2022-10-29 13:14:53 -0400

    Updated server port 9000

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static('public', {index: 'landing.html'}));
 
-const PORT = 8080;
+const PORT = 9000;
 app.listen(PORT, () => {
-	console.log(`Listening at http://localhost:${PORT}/`);
+	console.log(`Listening at https://gameoflife.duncantang.dev:${PORT}/`);
 });
